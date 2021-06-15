09a63e35dee65938c045c1367a8da1af4dcddc78
nissar <kotadia.ni@husky.neu.edu>
2021-06-15 10:53:41 -0700

    Configuration complete

--- src/main/java/com/egen/model/Order.java
+++ src/main/java/com/egen/model/Order.java
@@ -7,11 +7,19 @@ public class Order {
         this.id = id;
     }
 
+
     public String getId() {
         return id;
     }
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+            "id='" + id + '\'' +
+            '}';
     }
 }
